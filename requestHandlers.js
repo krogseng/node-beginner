0f12299a6f2790854d229a497f394b3528e4573e
Lynn <code@krogseng.com>
2017-02-13 06:55:08 -0800

    Attempted demo asyncness. Response error.

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,13 +1,13 @@
 const exec = require('child_process').exec;
 
 function start(response) {
-    console.log("Request handler 'start' was called.", response);
-
-    exec('ls -lah', function(error, stdout, stderr) {
-        response.writeHead(200, { "Content-Type": "text/plain" });
-        response.write(stdout);
-        response.end();
-    });
+    console.log("Request handler 'start' was called.");
+    exec('find /', { timeout: 100000, maxBuffer: 20000 * 1024 },
+        function(error, stdout, stderr) {
+            response.writeHead(200, { "Content-Type": "text/plain" });
+            response.write(stdout);
+            response.end();
+        });
 }
 
 function upload(response) {
